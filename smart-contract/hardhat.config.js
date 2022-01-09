4cf7a2f4d9100249dad3f909e3f1ff0f5fc1a336
Akim Benchiha <akim.benchiha@gmail.com>
2022-01-09 03:14:27 +0100

    Add ropsten test network

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -4,7 +4,13 @@
 require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
-const { GANACHE_LOCALHOST, GANACHE_PRIVATE_KEY_DEPLOYER } = process.env;
+const {
+  GANACHE_LOCALHOST,
+  GANACHE_PRIVATE_KEY_DEPLOYER,
+  ETHERSCAN_API_KEY,
+  ROPSTEN_PRIVATE_KEY_DEPLOYER,
+  ROPSTEN_URL,
+} = process.env;
 
 module.exports = {
   solidity: "0.8.0",
@@ -14,5 +20,12 @@ module.exports = {
       url: GANACHE_LOCALHOST,
       accounts: [GANACHE_PRIVATE_KEY_DEPLOYER],
     },
+    ropsten: {
+      url: ROPSTEN_URL,
+      accounts: [ROPSTEN_PRIVATE_KEY_DEPLOYER],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
